473284d6c188ae0ab641dfbf1c7a6f159bdf28d6
Jeremy <tempestj@sheridancollege.ca>
2025-03-05 08:38:44 -0500

    Commit #1 - created code to make ValueError test pass

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,11 @@
 """
 Validates strings as palindromes.
 """
+def main():
+    raise NotImplemented
+if __name__ == '__main__':
+    main()
+
+def is_palindrome(text: str)-> bool:
+    if not isinstance(text, str):
+        raise ValueError("Input must be a string")
