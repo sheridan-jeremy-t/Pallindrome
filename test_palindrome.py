f14b20cf6a81e015f1faa0acfd50b726e896523d
Jeremy <tempestj@sheridancollege.ca>
2025-03-05 08:53:33 -0500

    Commit #5 - created single character test

--- test_palindrome.py
+++ test_palindrome.py
@@ -16,3 +16,5 @@ def test_is_palindrome_string():
         is_palindrome([])
 def test_is_palindrome_string_empty():
     assert is_palindrome("") == False
+def test_is_palindrome_single_char():
+    assert is_palindrome("a") is True
