ace6958ff9b1da39cfab98e119ed16bf2790f25f
Jeremy <tempestj@sheridancollege.ca>
2025-03-05 08:34:38 -0500

    Commit #1 - created ValueError test

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,16 @@
 """
 Tests the palindrome module
 """
+import pytest
+from palindrome import *
+
+def test_is_palindrome_string():
+    """
+    Test that is_palindrome raises a ValueError when given anything but a string
+
+    :return: ValueError
+    """
+    with pytest.raises(ValueError):
+        is_palindrome(123)
+        is_palindrome(None)
+        is_palindrome([])
