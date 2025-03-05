f038e6f1fdcd606996fa2ea423fb3f373c8fd209
Jeremy <tempestj@sheridancollege.ca>
2025-03-05 09:14:11 -0500

    Commit #15 - created "Able was I ere I saw Elba" - test failed

--- test_palindrome.py
+++ test_palindrome.py
@@ -22,3 +22,4 @@ def test_is_palindrome_single_char():
     assert is_palindrome("abc") is False #commit #10 - test passed, no changes to code
     assert is_palindrome("laval") is True # commit #12 - test passed, no changes to code
     assert is_palindrome("toronto") is False #commit #14 - test passed, no changes to code
+    assert is_palindrome("Able was I ere I saw Elba") is True #commit #15 test failed
